974396f672939c8632e557ca83523c6a69f2822d
maelleeee1234 <maelle.bidet@etu.uca.fr>
2026-01-14 11:41:42 +0100

    texte histoire aikido

--- pages/histoire.php
+++ pages/histoire.php
@@ -6,6 +6,51 @@
     <title>Histoire</title>
 </head>
 <body>
-    
+<!-- HEADER -->
+<?php include 'header.php'; ?>
+
+
+<main class="histoire">
+
+    <!-- Aï Ki Do -->
+    <section class="aikido-signification">
+        <div class="mot">
+            <h2>Aï</h2>
+            <p>Harmonie</p>
+        </div>
+        <div class="mot">
+            <h2>Ki</h2>
+            <p>Énergie</p>
+        </div>
+        <div class="mot">
+            <h2>Do</h2>
+            <p>Voie</p>
+        </div>
+    </section>
+
+    <!-- Un art martial -->
+    <section class="texte">
+        <h3>Un art martial</h3>
+        <p>
+        L'Aïkido est un art martial japonais issu des budo traditionnels. Créé en 1925 par Morihei Ueshiba, c’est un sport basé sur la non-violence, l’harmonie et le respect. Cette discipline du corps et de l’esprit privilégie le développement personnel et la paix à la victoire.
+        L’Aïkido se pratique à mains nues, au sabre ou au bâton et se base sur des mouvements circulaires. Debout ou à genoux, l’objectif est de canaliser et neutraliser l'agressivité sans s’opposer directement. C’est une discipline mixte, sans compétition ni catégories qui met en avant l’entraide entre les pratiquants. La force et le poids ne sont pas déterminants et les gradés accompagnent et corrigent les débutants.		
+        </p>
+    </section>
+
+    <!-- Fondateur -->
+    <section class="fondateur">
+        <h3>Fondateur de l’Aïkido</h3>
+
+        <div class="fondateur-contenu">
+            <img src="img/fondateur.jpg" alt="Fondateur de l'Aïkido">
+            <div>
+                <p>
+                Né en 1883, c’est à l’âge de dix ans que Morihei Ueshiba commence les arts martiaux. Expert en ju-jitsu, ken-jutsu et daito-ryu au fil des années, il constate que le vrai intérêt des arts martiaux n’est pas de vaincre un adversaire par la force mais de garder la paix en acceptant et favorisant l’épanouissement des êtres. 
+                C’est grâce à toutes ces rencontres et expériences que naît l’Aïkido en 1925.  Morihei Ueshiba, également connu sous le nom de O’Sensei intègre cette dimension spirituelle à sa pratique et conçoit l’Aïkido comme une voie de paix et d’harmonie universelle.Lorsqu’il s’éteint en 1969, l’Aïkido était déjà répandu à travers le monde et était pratiqué par des centaines de milliers de personnes sur les cinq continents.				
+                </p>
+                <span class="signature">O sensei</span>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
